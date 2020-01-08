a5daa999c25a64a3b9e3ac88d1aa6de21beb2cbc
aerjayc <aerjay.castaneda@eee.upd.edu.ph>
2020-01-08 15:22:56 +0800

    can export pseudoGT as image

--- gt_io.py
+++ gt_io.py
@@ -1,17 +1,30 @@
 import numpy as np
+import cv2
+import os.path
 import h5py
+import matplotlib.pyplot as plt
+from pathlib import Path
+import image_proc
 
 
 def u2ToStr(u2, truncate_space=False):
-    if truncate_space:
-        s = ""
-        for c in u2:
-            if c == 32:
-                break
-            else:
-                s += chr(c)
-        return s
-    return "".join([chr(c[0]) for c in u2])
+    # if truncate_space:
+    #     s = ""
+    #     for c in u2:
+    #         if c == 32:
+    #             break
+    #         else:
+    #             s += chr(c)
+    #     return s
+
+    s = ""
+    for c in u2:
+        if truncate_space and c == 32:
+            break
+        else:
+            s += chr(c)
+    return s
+    #return "".join([chr(c[0]) for c in u2])
 
 def getCharBBxy(matpath):
     """ 3. charBB  : character-level bounding-boxes,
@@ -28,6 +41,7 @@ def getCharBBxy(matpath):
     # https://stackoverflow.com/questions/28541847/
     N = len(f['imnames'])
     for i in range(0, N):
+        i = 10000
         # filename
         imname_i = f[f['imnames'][i][0]]
         imname = u2ToStr(imname_i)
@@ -40,17 +54,56 @@ def getCharBBxy(matpath):
 
 
         print(f"filename: {imname}")
-        print(f"word lengths: {len}")
+        #print(f"word lengths: {len(charBB_i)}")
         print("Text:")
         # txt's in imname_i
         txt_i = np.array(f[f['txt'][i][0]]).T
         for instance in txt_i:
-            str_instance = u2ToStr(instance, truncate_space=True)
-            print(repr(f"\t{str_instance}"))
+            str_instance = u2ToStr(instance, truncate_space=False)
+            print("\t" + repr(str_instance))
 
         break
 
 
 if __name__ == '__main__':
-    # getCharBBxy('gt_17.mat')
-    pass
+    matpath = 'C:/Users/Aerjay/Downloads/SynthText/parts/gt_0.mat'
+    imgdir = 'C:/Users/Aerjay/Downloads/SynthText/SynthText/'
+    pseudoGT_dir = 'C:/Users/Aerjay/Downloads/SynthText/pseudoGT/'
+
+    f = h5py.File(matpath, 'r')
+
+    # https://stackoverflow.com/questions/28541847/
+    N = len(f['imnames'])
+    for i in range(0, N):
+        # filename
+        imname_i = f[f['imnames'][i][0]]
+        imname = u2ToStr(imname_i)
+        print(f"file: {imname}")
+        
+        pseudoGT_prefix, pseudoGT_ext = os.path.splitext(imname)
+        pseudoGT_name = pseudoGT_prefix + '_region' + pseudoGT_ext
+        pseudoGT_path = os.path.join(pseudoGT_dir, pseudoGT_name)
+        if os.path.isfile(pseudoGT_path):
+            print("\tAlready exists. Skipping...")
+            continue
+
+        # image shape
+        imgpath = os.path.join(imgdir, imname)
+        image_shape = cv2.imread(imgpath).shape[0:2]
+        # print(f"shape: (h,w) = {image_shape}")
+
+        # charBB's in imname_i
+        charBB_i = f[f['charBB'][i][0]]
+        pseudoGT_blank = np.zeros(image_shape)
+        pseudoGT_region = pseudoGT_blank.copy()
+        for charBB in charBB_i:
+            pseudoGT_region += image_proc.genDistortedGauss(charBB, img_size=image_shape)
+
+        Path(os.path.split(pseudoGT_path)[0]).mkdir(parents=True, exist_ok=True)
+        print(f"\tsaving to {pseudoGT_path}")
+        cv2.imwrite(pseudoGT_path, pseudoGT_region*255)
+        # print("showing image...")
+        # plt.figure()
+        # plt.imshow(pseudoGT_region, interpolation='nearest')
+        # plt.colorbar()
+        # plt.show()
--- image_proc.py
+++ image_proc.py
@@ -6,67 +6,79 @@ import cv2
 
 # https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
 def order_points(pts):
-	# initialzie a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype = "float32")
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "float32")
 
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
-	s = pts.sum(axis = 1)
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
 
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(pts, axis = 1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
 
-	# return the ordered coordinates
-	return rect
+    # return the ordered coordinates
+    return rect
 
-def four_point_transform(image, pts):
-	# obtain a consistent order of the points and unpack them
-	# individually
-	rect = order_points(pts)
-	(tl, tr, br, bl) = rect
+def perspectiveTransform(image, initial=None, final=None, size=None):
+    if type(initial) == type(None):
+        height, width = image.shape
+        initial = np.array([[0, 0],
+                            [width-1, 0],
+                            [width-1, height-1],
+                            [0, height-1]], dtype = "float32")
+    if type(final) == type(None):
+        maxWidth, maxHeight = getMaxSize(initial)
+        final = np.array([[0, 0],
+                          [maxWidth-1, 0],
+                          [maxWidth-1, maxHeight-1],
+                          [0, maxHeight-1]], dtype = "float32")
+    if type(size) == type(None):
+        w = max(final.T[0])
+        h = max(final.T[1])
+    else:
+        h, w = size
 
-	# compute the width of the new image, which will be the
-	# maximum distance between bottom-right and bottom-left
-	# x-coordiates or the top-right and top-left x-coordinates
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
+    initial = order_points(initial)
+    final = order_points(final)
 
-	# compute the height of the new image, which will be the
-	# maximum distance between the top-right and bottom-right
-	# y-coordinates or the top-left and bottom-left y-coordinates
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
+    # print(f"initial = {initial}")
+    # print(f"final = {final}")
+    # print(f"Width = {w}\nHeight = {h}")
 
-	# now that we have the dimensions of the new image, construct
-	# the set of destination points to obtain a "birds eye view",
-	# (i.e. top-down view) of the image, again specifying points
-	# in the top-left, top-right, bottom-right, and bottom-left
-	# order
-	dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
+    M = cv2.getPerspectiveTransform(initial, final)
+    warped = cv2.warpPerspective(image, M, (w, h))
 
-	# compute the perspective transform matrix and then apply it
-	M = cv2.getPerspectiveTransform(rect, dst)
-	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    return warped
 
-	# return the warped image
-	return warped
+def getMaxSize(BBcoords):
+    (tl, tr, br, bl) = BBcoords
 
-def genDistortedGauss(BBcoords):
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    return maxWidth, maxHeight
+
+def genDistortedGauss(BBcoords, img_size):
     """ BBcoords = 4x2 array
     """
 
@@ -76,31 +88,42 @@ def genDistortedGauss(BBcoords):
     variance = 1
     height = np.sqrt(2*np.pi*variance)
 
-    size = 25   # depends on the max edge length of charBB
-    bounds = 3
+    size = max(getMaxSize(BBcoords))
+    bounds = 2.5
 
     x = np.linspace(-bounds, bounds, size)
     x, y = np.meshgrid(x,x)
     gauss = height * (1/np.sqrt(2*np.pi*variance)) * np.exp(-((x - x_mean)**2 + (y - y_mean)**2)/(2*variance))
 
-    plt.figure()
-    plt.imshow(gauss, interpolation='nearest')
-    plt.colorbar()
-    #plt.show()
+    # plt.figure()
+    # plt.imshow(gauss, interpolation='nearest')
+    # plt.colorbar()
 
-    distorted_gauss = four_point_transform(gauss, BBcoords)
-    
-    plt.figure()
-    plt.imshow(distorted_gauss, interpolation='nearest')
-    plt.colorbar()
-    plt.show()
+    distorted_gauss = perspectiveTransform(gauss, final=BBcoords, size=img_size)
 
     return distorted_gauss
 
 
 if __name__ == '__main__':
-    pts = np.array([(5, 5),
-                    (15, 7),
-                    (20, 15),
-                    (7, 20)])
-    genDistortedGauss(pts)
+    plt.figure()
+    plt.imshow(cv2.imread('images/warped.jpg', 0), cmap='gray', interpolation='nearest')
+
+    BBcoords = np.array([ (15,11),
+                          (48,24),
+                          (52,47),
+                          (19,62) ])
+
+    a = genDistortedGauss(BBcoords, img_size=(100,100))
+
+    BBcoords = np.array([ (50,20),
+                          (80,20),
+                          (80,50),
+                          (50,50) ])
+
+    b = genDistortedGauss(BBcoords, img_size=(100,100))
+
+    plt.figure()
+    plt.imshow(a+b, interpolation='nearest')
+    plt.colorbar()
+
+    plt.show()
