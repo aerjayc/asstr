8fbc1695603181630a41ee1f2f473c209207426e
aerjayc <aerjay.castaneda@eee.upd.edu.ph>
2020-01-10 12:36:06 +0800

    working order_points, reduced comments and redundant functions

--- gt_io.py
+++ gt_io.py
@@ -7,65 +7,8 @@ from pathlib import Path
 import image_proc
 
 
-def u2ToStr(u2, truncate_space=False):
-    # if truncate_space:
-    #     s = ""
-    #     for c in u2:
-    #         if c == 32:
-    #             break
-    #         else:
-    #             s += chr(c)
-    #     return s
-
-    s = ""
-    for c in u2:
-        if truncate_space and c == 32:
-            break
-        else:
-            s += chr(c)
-    return s
-    #return "".join([chr(c[0]) for c in u2])
-
-def getCharBBxy(matpath):
-    """ 3. charBB  : character-level bounding-boxes,
-                    each represented by a tensor of size 2x4xNCHARS_i
-                    - the first dimension is 2 for x and y respectively,
-                    - the second dimension corresponds to the 4 points
-                        (clockwise, starting from top-left), and
-                    -  the third dimension of size NCHARS_i, corresponds to
-                        the number of characters in the i_th image.
-    """
-
-    f = h5py.File(matpath, 'r')
-
-    # https://stackoverflow.com/questions/28541847/
-    N = len(f['imnames'])
-    for i in range(0, N):
-        i = 10000
-        # filename
-        imname_i = f[f['imnames'][i][0]]
-        imname = u2ToStr(imname_i)
-
-        # charBB's in imname_i
-        charBB_i = f[f['charBB'][i][0]]
-
-        # wordBB's in imname_i
-        wordBB_i = f[f['wordBB'][i][0]]
-
-
-        print(f"filename: {imname}")
-        #print(f"word lengths: {len(charBB_i)}")
-        print("Text:")
-        # txt's in imname_i
-        txt_i = np.array(f[f['txt'][i][0]]).T
-        for instance in txt_i:
-            str_instance = u2ToStr(instance, truncate_space=False)
-            print("\t" + repr(str_instance))
-
-        break
-
-
 if __name__ == '__main__':
+    skip_existing = False
     matpath = 'C:/Users/Aerjay/Downloads/SynthText/parts/gt_0.mat'
     imgdir = 'C:/Users/Aerjay/Downloads/SynthText/SynthText/'
     pseudoGT_dir = 'C:/Users/Aerjay/Downloads/SynthText/pseudoGT/'
@@ -77,27 +20,25 @@ if __name__ == '__main__':
     for i in range(0, N):
         # filename
         imname_i = f[f['imnames'][i][0]]
-        imname = u2ToStr(imname_i)
+        imname = image_proc.u2ToStr(imname_i)
         print(f"file: {imname}")
         
         pseudoGT_prefix, pseudoGT_ext = os.path.splitext(imname)
-        pseudoGT_name = pseudoGT_prefix + '_region' + pseudoGT_ext
+        pseudoGT_name = pseudoGT_prefix + '_region.png' #+ pseudoGT_ext
         pseudoGT_path = os.path.join(pseudoGT_dir, pseudoGT_name)
-        if os.path.isfile(pseudoGT_path):
+        if skip_existing and os.path.isfile(pseudoGT_path):
             print("\tAlready exists. Skipping...")
             continue
 
         # image shape
         imgpath = os.path.join(imgdir, imname)
         image_shape = cv2.imread(imgpath).shape[0:2]
-        # print(f"shape: (h,w) = {image_shape}")
 
         # charBB's in imname_i
         charBB_i = f[f['charBB'][i][0]]
-        pseudoGT_blank = np.zeros(image_shape)
-        pseudoGT_region = pseudoGT_blank.copy()
-        for charBB in charBB_i:
-            pseudoGT_region += image_proc.genDistortedGauss(charBB, img_size=image_shape)
+        # text in imname_i
+        txt_i = f[f['txt'][i][0]]
+        pseudoGT_region, pseudoGT_affinity = image_proc.genPseudoGT(charBB_i, txt_i, image_shape)
 
         Path(os.path.split(pseudoGT_path)[0]).mkdir(parents=True, exist_ok=True)
         print(f"\tsaving to {pseudoGT_path}")
@@ -106,4 +47,8 @@ if __name__ == '__main__':
         # plt.figure()
         # plt.imshow(pseudoGT_region, interpolation='nearest')
         # plt.colorbar()
+        # plt.figure()
+        # plt.imshow(pseudoGT_affinity, interpolation='nearest')
+        # plt.colorbar()
         # plt.show()
+        # break
--- image_proc.py
+++ image_proc.py
@@ -4,30 +4,86 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 import cv2
 
-# https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+# matfile-specific functions
+def u2ToStr(u2, truncate_space=False):
+    s = ""
+    for c in u2:
+        if truncate_space and c == 32:
+            break
+        else:
+            s += chr(c)
+    return s
+
+# general, low-level image-processing functions
+def getPixelAngle(origin, pt, units='radians'):
+    pt_o = pt - origin
+
+    if pt_o[0] == 0:
+        angle = np.pi/2
+    elif pt_o[0] > 0:
+        angle = np.arctan(pt_o[1]/pt_o[0])
+    else:
+        angle = np.arctan(pt_o[1]/pt_o[0]) + np.pi
+    
+    if units != 'radians':
+        angle *= 180/np.pi
+
+    return angle
+
+def getPixelAngles(origin, pts, units='radians'):
+    angles = np.zeros(pts.shape[0], dtype='float32')
+    for i, pt in enumerate(pts):
+        angles[i] = getPixelAngle(origin, pt, units=units)
+
+    return angles
+
+def getAffinityBB(charBB1, charBB2):
+    edge1 = getAffinityEdge(charBB1)
+    edge2 = getAffinityEdge(charBB2)
+    return np.concatenate((edge1, edge2))
+
+def getAffinityEdge(charBB):
+    charBB = order_points(charBB)
+    BB_center = getCentroid(charBB)
+    tri1 = np.array([charBB[0], charBB[1], BB_center])
+    tri1_center = getCentroid(tri1)
+    tri2 = np.array([charBB[2], charBB[3], BB_center])
+    tri2_center = getCentroid(tri2)
+
+    return np.array([tri1_center, tri2_center])
+
+def getCentroid(vertices):
+    return np.mean(vertices, axis=0)
+
+def getMaxSize(BBcoords):
+    # from https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+    (tl, tr, br, bl) = BBcoords
+
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    return maxWidth, maxHeight
+
 def order_points(pts):
-    # initialzie a list of coordinates that will be ordered
-    # such that the first entry in the list is the top-left,
-    # the second entry is the top-right, the third is the
-    # bottom-right, and the fourth is the bottom-left
-    rect = np.zeros((4, 2), dtype = "float32")
+    centroid = getCentroid(pts)
+    angles = getPixelAngles(centroid, pts)
+    ordered_pts = pts[len(pts)-1 - np.argsort(angles)]
+    ordered_pts = ordered_pts[np.arange(len(pts))-1].astype('float32')
 
-    # the top-left point will have the smallest sum, whereas
-    # the bottom-right point will have the largest sum
-    s = pts.sum(axis = 1)
-    rect[0] = pts[np.argmin(s)]
-    rect[2] = pts[np.argmax(s)]
+    return ordered_pts
 
-    # now, compute the difference between the points, the
-    # top-right point will have the smallest difference,
-    # whereas the bottom-left will have the largest difference
-    diff = np.diff(pts, axis = 1)
-    rect[1] = pts[np.argmin(diff)]
-    rect[3] = pts[np.argmax(diff)]
-
-    # return the ordered coordinates
-    return rect
-
+# high-level image-processing functions
 def perspectiveTransform(image, initial=None, final=None, size=None):
     if type(initial) == type(None):
         height, width = image.shape
@@ -50,39 +106,12 @@ def perspectiveTransform(image, initial=None, final=None, size=None):
     initial = order_points(initial)
     final = order_points(final)
 
-    # print(f"initial = {initial}")
-    # print(f"final = {final}")
-    # print(f"Width = {w}\nHeight = {h}")
-
     M = cv2.getPerspectiveTransform(initial, final)
     warped = cv2.warpPerspective(image, M, (w, h))
 
     return warped
 
-def getMaxSize(BBcoords):
-    (tl, tr, br, bl) = BBcoords
-
-    # compute the width of the new image, which will be the
-    # maximum distance between bottom-right and bottom-left
-    # x-coordiates or the top-right and top-left x-coordinates
-    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-    maxWidth = max(int(widthA), int(widthB))
-
-    # compute the height of the new image, which will be the
-    # maximum distance between the top-right and bottom-right
-    # y-coordinates or the top-left and bottom-left y-coordinates
-    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-    maxHeight = max(int(heightA), int(heightB))
-
-    return maxWidth, maxHeight
-
 def genDistortedGauss(BBcoords, img_size):
-    """ BBcoords = 4x2 array
-    """
-
-    # make 2d isotropic Gaussian
     x_mean = 0
     y_mean = 0
     variance = 1
@@ -102,6 +131,45 @@ def genDistortedGauss(BBcoords, img_size):
     distorted_gauss = perspectiveTransform(gauss, final=BBcoords, size=img_size)
 
     return distorted_gauss
+
+def getBreakpoints(txt):
+    cumulative = -1
+    breakpoints = []
+    for instance in txtToInstance(txt):
+        cumulative += len(instance)
+        breakpoints += [cumulative]
+
+    return breakpoints
+
+def txtToInstance(txt):
+    txt_i = np.array(txt).T
+    instances = []
+    for instance in txt_i:
+        instance = u2ToStr(instance)
+        instances += instance.split()
+    
+    return instances
+
+def genPseudoGT(charBB_i, txt, image_shape, generate_affinity=True):
+    breakpoints = getBreakpoints(txt)
+    # instances = txtToInstance(txt)
+    # entire_string = ''.join(instances)
+
+    pseudoGT_blank = np.zeros(image_shape, dtype='float')
+    pseudoGT_region = pseudoGT_blank.copy()
+    pseudoGT_affinity = pseudoGT_blank.copy()
+    charBB_prev = None
+    for j, charBB in enumerate(charBB_i):
+        pseudoGT_region += genDistortedGauss(charBB, img_size=image_shape)
+        
+        # if prev char is not a breakpoint = if curr char same instance as prev
+        if ((j-1) not in breakpoints) and (j > 0) and generate_affinity:
+            affinityBB = order_points(getAffinityBB(charBB_prev, charBB))
+            pseudoGT_affinity += genDistortedGauss(affinityBB, img_size=image_shape)
+        charBB_prev = charBB
+
+    return pseudoGT_region, pseudoGT_affinity
+
 
 
 if __name__ == '__main__':
