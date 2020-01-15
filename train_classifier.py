e8ad9ad79a5fe2d64c8f680f2486fe0a63f0205c
aerjayc <aerjay.castaneda@eee.upd.edu.ph>
2020-01-15 13:20:44 +0800

    working simpleGenChar

---
+++ train_classifier.py
@@ -0,0 +1,84 @@
+import numpy as np
+import cv2
+import image_proc
+import random
+import h5py
+import os.path
+import matplotlib.pyplot as plt
+
+
+def genChar():
+    # 1. randomly select character + GT from dataset by:
+        # a. select matfile at random
+        # b. select image file at random
+        # c. select character at random
+    # 2. use
+
+    # get random integer i : 0 <= i <= 17
+        # matfname = gt_{i}.mat
+    # get random integer j : 0 <= j <= len(gt_i)
+    # get random integer k : 0 <= k <= len(f[f['charBB'][i][0]])
+    matpath = ''    # randomly selected
+    f = h5py.File(matpath, 'r')
+    charBB = f[f['charBB'][0][0]]
+    pass
+    # return cropped, char
+
+def simpleGenChar(N, img_dir, mat_dir, N_images=858750, matboundary=50000):
+    # generate a sequence of N integers <= 858750
+    for i in random.sample(range(N_images), N):
+        # determine matnum: i in gt_{matnum}.mat
+        matnum = int(i/50000)
+        j = i % matboundary
+        
+        matpath = os.path.join(mat_dir, f"gt_{matnum}.mat")
+        # print(f"matpath = {matpath}")
+
+        f = h5py.File(matpath, 'r')
+        string = "".join(image_proc.txtToInstance(f[f['txt'][j][0]]))
+        k = random.randint(0, len(string)-1)
+        # print(f"string = {string}\nk = {k}")
+        char = string[k]
+        charBB = f[f['charBB'][j][0]][k]
+        # print(f"char = {char}")
+
+        imname = image_proc.u2ToStr(f[f['imnames'][j][0]])
+        img_path = os.path.join(img_dir, imname)
+        # print(f"img_path = {img_path}")
+        
+        cropped = getCroppedImage(img_path, charBB)
+        
+        # print("showing image...")
+        # plt.figure()
+        # plt.imshow(cropped, interpolation='nearest')
+        # plt.show()
+
+        yield cropped, char
+
+
+def getCroppedImage(img_path, coords):
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    cropped = image_proc.perspectiveTransform(img, initial=coords)
+    return cropped
+
+# given matfile, entry number, character number:
+# return image, character, image name
+def getSample(f, entry_no, char_no, image_dir):
+    imname = image_proc.u2ToStr(f[f['charBB'][entry_no][0]])
+    image_path = os.path.join(image_dir, imname)
+
+    charBB = f[f['charBB'][entry_no][0]][char_no]
+    image = getCroppedImage(image_path, charBB)
+
+    instances = image_proc.txtToInstance(f[f['txt'][entry_no][0]])
+    string = ''.join(instances)
+    char = string[char_no]
+
+    return image, char, imname
+
+
+if __name__ == '__main__':
+    img_dir = 'C:/Users/Aerjay/Downloads/SynthText/SynthText'
+    mat_dir = 'C:/Users/Aerjay/Downloads/SynthText/parts'
+    # simpleGenChar(10, img_dir, mat_dir, N_images=100)
+    pass
