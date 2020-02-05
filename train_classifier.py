d1a63f158dcd29b72ca903588602cb8431cf2669
aerjayc <aerjay.castaneda@eee.upd.edu.ph>
2020-02-05 10:53:44 +0800

    working balanced char img generator

--- train_classifier.py
+++ train_classifier.py
@@ -4,6 +4,7 @@ import image_proc
 import random
 import h5py
 import os.path
+from pathlib import Path
 import matplotlib.pyplot as plt
 
 
@@ -24,39 +25,99 @@ def genChar():
     pass
     # return cropped, char
 
-def simpleGenChar(N, img_dir, mat_dir, N_images=858750, matboundary=50000):
-    # generate a sequence of N integers <= 858750
+def genBalancedCharDataset(N_max, img_dir, mat_dir, char_dir, skip_existing=True,
+                           classes=None, N_images = 858750, matboundary=50000):
+    """ randomly select images forever
+        for each selected image, save the character img
+        and update the distribution
+    """
+    if not classes: # default classes:
+        classes = list("AaBbCDdEeFfGgHhIiJjKLlMmNnOPQqRrSTtUVWXYZ")
+    distribution = {c:0 for c in classes}
+            # initialize distribution to the already-saved images
+
+    # generate a sequence of N integers < 858750
+    for i in random.sample(range(N_images), N_images):
+        if min(distribution.values()) >= N_max:
+            break
+        # determine matnum: i in gt_{matnum}.mat
+        matnum = int(i/50000)
+        matpath = os.path.join(mat_dir, f"gt_{matnum}.mat")
+        f = h5py.File(matpath, 'r')
+
+        # determine the image number wrt matnum
+        j = i % matboundary
+        string = "".join(image_proc.txtToInstance(f[f['txt'][j][0]]))
+        imname = image_proc.u2ToStr(f[f['imnames'][j][0]])
+        img_path = os.path.join(img_dir, imname)
+        #print(f"gt_{matnum}.mat\t {img_path}") #
+
+        for k, char in enumerate(string):
+            if char not in distribution:
+                continue
+            if distribution[char] >= N_max:
+                continue
+            # determine output img path
+            char_path = os.path.join(char_dir, char, f"{char}_{i}_{j}_{k}.png")
+            print(char_path, end='')
+            Path(os.path.split(char_path)[0]).mkdir(parents=True, exist_ok=True)
+            if skip_existing and os.path.isfile(char_path):
+                print("\t Skipping...")
+                continue
+            print("")
+
+            # write img to path
+            charBB = f[f['charBB'][j][0]][k]
+            char_img = getCroppedImage(img_path, charBB)
+            cv2.imwrite(char_path, char_img)
+
+            distribution[char] += 1
+
+    print(distribution)
+
+
+def simpleGenChar(N, img_dir, mat_dir, N_images=850000, matboundary=50000):
+    # generate a sequence of N integers < 858750
     for i in random.sample(range(N_images), N):
         # determine matnum: i in gt_{matnum}.mat
         matnum = int(i/50000)
         j = i % matboundary
-        
+
         matpath = os.path.join(mat_dir, f"gt_{matnum}.mat")
-        # print(f"matpath = {matpath}")
+        # print(f"matpath = {matpath}") #
 
         f = h5py.File(matpath, 'r')
         string = "".join(image_proc.txtToInstance(f[f['txt'][j][0]]))
         k = random.randint(0, len(string)-1)
-        # print(f"string = {string}\nk = {k}")
+        # print(f"string = {string}\nk = {k}") #
         char = string[k]
         charBB = f[f['charBB'][j][0]][k]
-        # print(f"char = {char}")
+        print(f"char = {char}") #
 
         imname = image_proc.u2ToStr(f[f['imnames'][j][0]])
         img_path = os.path.join(img_dir, imname)
-        # print(f"img_path = {img_path}")
-        
+        # print(f"img_path = {img_path}") #
+
         cropped = getCroppedImage(img_path, charBB)
-        
-        # print("showing image...")
-        # plt.figure()
-        # plt.imshow(cropped, interpolation='nearest')
-        # plt.show()
 
-        yield cropped, char
+        print("showing image...") #
+        plt.figure() #
+        plt.imshow(cropped, interpolation='nearest') #
+        plt.show() #
+
+        # yield cropped, char
 
 
-def getCroppedImage(img_path, coords):
+def getCroppedImage(img_path, coords, augment=False):
+    if augment:
+        # perform augmentation
+        return
+    #else:
+        #print("NO AUGMENTATION")
+    if not os.path.isfile(img_path):
+        print(f"WARNING: {img_path} DOES NOT EXIST. Skipping...")
+        return None
+
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
     cropped = image_proc.perspectiveTransform(img, initial=coords)
     return cropped
@@ -80,5 +141,7 @@ def getSample(f, entry_no, char_no, image_dir):
 if __name__ == '__main__':
     img_dir = 'C:/Users/Aerjay/Downloads/SynthText/SynthText'
     mat_dir = 'C:/Users/Aerjay/Downloads/SynthText/parts'
+    char_dir = 'C:/Users/Aerjay/Downloads/SynthText/chars'
+    classes = ['a', 'e', 'i', 'o', 'u']
     # simpleGenChar(10, img_dir, mat_dir, N_images=100)
-    pass
+    genBalancedCharDataset(20, img_dir, mat_dir, char_dir, classes=classes)
