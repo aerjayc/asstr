fa855e901d50dacbb706902b8c98df5761579a59
aerjayc <aerjay.castaneda@eee.upd.edu.ph>
2020-06-03 13:18:07 +0800

    Return BBs instead of indices

--- character_grouper.py
+++ character_grouper.py
@@ -17,9 +17,9 @@ def character_grouper(charBBs, k=0.7):
                     same word
     """
 
-    lines = {}
     skip_BBs = []
     line_num = 0
+    lines = []
     for i, charBB in enumerate(charBBs):
         if i in skip_BBs:
             continue
@@ -27,7 +27,7 @@ def character_grouper(charBBs, k=0.7):
         top = np.min(charBB, axis=0)[1]
         bottom = np.max(charBB, axis=0)[1]
 
-        lines[line_num] = [i]
+        lines.append([charBB])
 
         for j, BB in enumerate(charBBs):
             if i == j:
@@ -39,31 +39,34 @@ def character_grouper(charBBs, k=0.7):
 
             if top <= center_y <= bottom:   # top has lower y-value
                 # same line
-                lines[line_num].append(j)
                 skip_BBs.append(j)
+                lines[line_num].append(BB)
 
+        lines[line_num] = np.array(lines[line_num])
         line_num += 1
 
-    words = {}
+    # sort each line    # line: (n_chars, 4, 2)
+    for i, line in enumerate(lines):
+        sort_x = line[:,0,0].argsort()
+        lines[i] = line[sort_x]
+
+    words = []
     word_num = -1
-    j = 0
-    for i, line in enumerate(lines.values()):
+    for i, line in enumerate(lines):
         # get threshold for current line
         sum_width = 0
-        for idx in line:
-            charBB = charBBs[idx]
-            width = ((charBB[1][0] - charBB[0][0]) + (charBB[2][0] - charBB[3][0])) / 2.0
+        for charBB in line:     # get average width in line
+            width = ((charBB[1][0] - charBB[0][0]) +
+                     (charBB[2][0] - charBB[3][0])) / 2.0
             sum_width += width
         threshold = sum_width / len(line)
 
         # loop over all chars in current line
         prev_charBB = None
-        for idx in line:
-            charBB = charBBs[idx]
-            if prev_charBB is None:
+        for charBB in line:
+            if prev_charBB is None:     # first iteration
                 word_num += 1
-                words[word_num] = [j]
-                j += 1
+                words.append([charBB])
                 prev_charBB = charBB
                 continue
 
@@ -71,17 +74,31 @@ def character_grouper(charBBs, k=0.7):
             left_curr = np.min(charBB, axis=0)[0]
             if (left_curr - right_prev) <= k*threshold:
                 # same word
-                words[word_num].append(j)
+                words[word_num].append(charBB)
             else:
                 # new word
                 word_num += 1
-                words[word_num] = [j]
+                words.append([charBB])
 
-            j += 1
             prev_charBB = charBB
+
+    for i in range(len(words)):
+        words[i] = np.array(words[i])
 
     return lines, words
 
+def words_to_wordBBs(words):
+    wordBBs = np.zeros((len(words), 4, 2))
+    for i, word in enumerate(words):
+        coords = word.reshape(-1,2)
+        tl = np.min(coords, axis=0)
+        br = np.max(coords, axis=0)
+        tr = np.array([br[0], tl[1]])
+        bl = np.array([tl[0], br[1]])
+
+        wordBBs[i] = np.array([tl, tr, br, bl])
+
+    return wordBBs.astype('int')
 
 def main(i=0):
     headers = ['R', 'G' ,'B' ,  # RGB values
